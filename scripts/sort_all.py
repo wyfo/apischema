4777083fc2d4de2bd1ef9ac1db67dfafb73993b7
Joseph Perez <joperez@hotmail.fr>
2020-10-10 18:15:26 +0200

    Add script to sort __all__ in pre-commit

---
+++ scripts/sort_all.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+import os
+from pathlib import Path
+from subprocess import run
+
+
+PACKAGE_DIR = Path(__file__).parent / "apischema"
+
+for root, dirs, files in os.walk(PACKAGE_DIR):
+    for filename in files:
+        if not filename.endswith(".py"):
+            continue
+        path = f"{root}/{filename}"
+        with open(path) as f:
+            lines = f.readlines()
+        try:
+            all_first_line = next(i for i, l in enumerate(lines) if "__all__ = [" in l)
+        except StopIteration:
+            continue
+        all_last_line = next(
+            i + all_first_line for i, l in enumerate(lines[all_first_line:]) if "]" in l
+        )
+        namespace: dict = {}
+        exec("".join(lines[all_first_line : all_last_line + 1]), namespace)
+        __all__ = ", ".join(f'"{s}"' for s in sorted(namespace["__all__"]))
+        with open(path, "w") as f:
+            f.writelines(lines[:all_first_line])
+            f.write(f"__all__ = [{__all__}]\n")
+            f.writelines(lines[all_last_line + 1 :])
+        run(["black", path])
