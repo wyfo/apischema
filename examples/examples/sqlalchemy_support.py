b721328f40afdb3811e54dce9b677c445f299f58
Joseph Perez <joperez@hotmail.fr>
2020-12-13 16:03:16 +0100

    Improve doc

--- examples/examples/sqlalchemy_support.py
+++ examples/examples/sqlalchemy_support.py
@@ -1,12 +1,22 @@
-from dataclasses import make_dataclass
+from dataclasses import MISSING, make_dataclass
 from inspect import getmembers
+from typing import Collection
 
+from graphql import print_schema
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import as_declarative
 
-from apischema import Undefined, deserialize, serialize
+from apischema import Undefined, deserialize, graphql_schema, serialize
 from apischema.conversions.dataclass_model import dataclass_model
 from apischema.json_schema import serialization_schema
+
+
+def has_default(column: Column) -> bool:
+    return (
+        column.nullable
+        or column.default is not None
+        or column.server_default is not None
+    )
 
 
 # Very basic SQLAlchemy support
@@ -18,24 +28,46 @@ class Base:
             return
 
         fields = [
-            (column.name or field_name, column.type.python_type, Undefined)
+            (
+                column.name or field_name,
+                column.type.python_type,
+                Undefined if has_default(column) else MISSING,
+            )
             for field_name, column in columns
         ]
         dataclass_model(cls)(make_dataclass(cls.__name__, fields))
 
 
-class A(Base):
-    __tablename__ = "a"
-    key = Column(Integer, primary_key=True)
+class Foo(Base):
+    __tablename__ = "foo"
+    bar = Column(Integer, primary_key=True)
 
 
-a = deserialize(A, {"key": 0})
-assert isinstance(a, A)
-assert a.key == 0
-assert serialize(a) == {"key": 0}
-assert serialization_schema(A) == {
+foo = deserialize(Foo, {"bar": 0})
+assert isinstance(foo, Foo)
+assert foo.bar == 0
+assert serialize(foo) == {"bar": 0}
+assert serialization_schema(Foo) == {
     "$schema": "http://json-schema.org/draft/2019-09/schema#",
     "type": "object",
-    "properties": {"key": {"type": "integer"}},
+    "properties": {"bar": {"type": "integer"}},
+    "required": ["bar"],
     "additionalProperties": False,
 }
+
+
+def foos() -> Collection[Foo]:
+    ...
+
+
+schema = graphql_schema(query=[foos])
+schema_str = """\
+type Query {
+  foos: [Foo!]
+}
+
+type Foo {
+  bar: Int!
+}
+"""
+assert print_schema(schema) == schema_str
