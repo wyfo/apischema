93ea0b585962d7b91704263c2bbe8087186ce562
Joseph Perez <joperez@hotmail.fr>
2021-03-10 08:46:41 +0100

    Fix schema_ref handling when conversion in GraphQL schema
    schema_ref was previously discarded in case of conversion

---
+++ tests/test_default_conversion_schema_ref.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+
+from graphql.utilities import print_schema
+
+from apischema import serializer
+from apischema.graphql import graphql_schema
+from apischema.json_schema import serialization_schema
+
+
+@dataclass
+class A:
+    a: int
+
+
+@dataclass
+class B:
+    b: int
+
+    @serializer
+    def to_a(self) -> A:
+        return A(self.b)
+
+
+def b() -> B:
+    return B(0)
+
+
+def test_default_conversion_schema_ref():
+    assert (
+        print_schema(graphql_schema(query=[b]))
+        == """\
+type Query {
+  b: B!
+}
+
+type B {
+  a: Int!
+}
+"""
+    )
+    assert serialization_schema(B, all_refs=True) == {
+        "$ref": "#/$defs/B",
+        "$defs": {
+            "B": {"$ref": "#/$defs/A"},
+            "A": {
+                "type": "object",
+                "properties": {"a": {"type": "integer"}},
+                "required": ["a"],
+                "additionalProperties": False,
+            },
+        },
+        "$schema": "http://json-schema.org/draft/2019-09/schema#",
+    }
+    assert serialization_schema(B) == {
+        "type": "object",
+        "properties": {"a": {"type": "integer"}},
+        "required": ["a"],
+        "additionalProperties": False,
+        "$schema": "http://json-schema.org/draft/2019-09/schema#",
+    }
