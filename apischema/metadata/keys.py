df84802f8c4bbbf000f24dea28d22a7c03f59449
Joseph Perez <joperez@hotmail.fr>
2021-04-03 02:49:40 +0200

    Refacto annotated_metadata

--- apischema/metadata/keys.py
+++ apischema/metadata/keys.py
@@ -1,14 +1,4 @@
-from dataclasses import Field
-from typing import Mapping
-
-from apischema.types import AnyType, ChainMap
-from apischema.typing import get_args, get_origin
 from apischema.utils import PREFIX
-
-try:
-    from apischema.typing import Annotated
-except ImportError:
-    Annotated = ...  # type: ignore
 
 ALIAS_METADATA = f"{PREFIX}alias"
 ALIAS_NO_OVERRIDE_METADATA = f"{PREFIX}alias_no_override"
@@ -23,24 +13,3 @@ REQUIRED_METADATA = f"{PREFIX}required"
 SCHEMA_METADATA = f"{PREFIX}schema"
 SKIP_METADATA = f"{PREFIX}skip"
 VALIDATORS_METADATA = f"{PREFIX}validators"
-
-FORBIDDEN_WITH_AGGREGATE = {
-    ALIAS_METADATA,
-    ALIAS_NO_OVERRIDE_METADATA,
-    DEFAULT_AS_SET_METADATA,
-    POST_INIT_METADATA,
-    REQUIRED_METADATA,
-    SCHEMA_METADATA,
-    VALIDATORS_METADATA,
-}
-
-
-def is_aggregate_field(field: Field) -> bool:
-    return MERGED_METADATA in field.metadata or PROPERTIES_METADATA in field.metadata
-
-
-def get_annotated_metadata(tp: AnyType) -> Mapping:
-    if get_origin(tp) == Annotated:
-        return ChainMap(*(arg for arg in get_args(tp)[1:] if isinstance(arg, Mapping)))
-    else:
-        return {}
