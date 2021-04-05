2e4e9f49ca9a82b10d0c9507f8f1ac716ca41a85
Joseph Perez <joperez@hotmail.fr>
2021-04-05 13:22:54 +0200

    Add doc

--- examples/schema_extra.py
+++ examples/schema_extra.py
@@ -5,25 +5,33 @@ from apischema.json_schema import deserialization_schema
 
 Foo = NewType("Foo", str)
 schema(min_len=20, extra={"typeName": "Foo"})(Foo)
-
-Bar = NewType("Bar", int)
-schema(
-    extra={"$ref": "http://some-domain.org/path/tp/schema.json#/$defs/Bar"},
-    override=True,
-)(Bar)
-
 assert deserialization_schema(Foo) == {
     "$schema": "http://json-schema.org/draft/2019-09/schema#",
     "type": "string",
     "minLength": 20,
     "typeName": "Foo",
 }
+
+Bar = NewType("Bar", str)
+schema(min_len=20, extra=lambda s: s.update({"typeName": "Bar"}))(Bar)
 assert deserialization_schema(Bar) == {
     "$schema": "http://json-schema.org/draft/2019-09/schema#",
-    "$ref": "http://some-domain.org/path/tp/schema.json#/$defs/Bar",
+    "type": "string",
+    "minLength": 20,
+    "typeName": "Bar",
+}
+
+Baz = NewType("Baz", int)
+schema(
+    extra={"$ref": "http://some-domain.org/path/to/schema.json#/$defs/Baz"},
+    override=True,
+)(Baz)
+assert deserialization_schema(Baz) == {
+    "$schema": "http://json-schema.org/draft/2019-09/schema#",
+    "$ref": "http://some-domain.org/path/to/schema.json#/$defs/Baz",
 }
 # Without override=True, it would be {
 #     "$schema": "http://json-schema.org/draft/2019-09/schema#",
-#     "$ref": "http://some-domain.org/path/tp/schema.json#/$defs/Bar",
+#     "$ref": "http://some-domain.org/path/to/schema.json#/$defs/Baz",
 #     "type": "integer",
 # }
