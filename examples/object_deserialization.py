5824cb103badf898ac4e280670b048cd9ca9f5e4
wyfo <joperez@hotmail.fr>
2021-06-06 00:55:12 +0200

    Add class modifiers to object conversions (#148)

--- examples/object_deserialization.py
+++ examples/object_deserialization.py
@@ -1,20 +1,17 @@
-from collections.abc import Iterable
-from typing import Annotated
-
-from apischema import deserialize, type_name
+from apischema import deserialize, deserializer, type_name
 from apischema.json_schema import deserialization_schema
-from apischema.metadata import conversion
 from apischema.objects import object_deserialization
 
 
-def create_range(start: int, stop: int, step: int = 1) -> Iterable[int]:
+def create_range(start: int, stop: int, step: int = 1) -> range:
     return range(start, stop, step)
 
 
-range_conv = object_deserialization(create_range, type_name=type_name("Range"))
-Range = Annotated[Iterable[int], conversion(deserialization=range_conv)]
-assert deserialize(Range, {"start": 0, "stop": 10}) == range(0, 10)
-assert deserialization_schema(Range) == {
+range_conv = object_deserialization(create_range, type_name("Range"))
+# Conversion can be registered
+deserializer(range_conv)
+assert deserialize(range, {"start": 0, "stop": 10}) == range(0, 10)
+assert deserialization_schema(range) == {
     "$schema": "http://json-schema.org/draft/2019-09/schema#",
     "type": "object",
     "properties": {
