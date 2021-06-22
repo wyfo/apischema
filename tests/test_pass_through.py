cda5f274c39a4ebebfb3a26ee9df821eae6b2e02
Joseph Perez <joperez@hotmail.fr>
2021-06-22 11:02:43 +0200

    Add pass_through test for dataclasses options

---
+++ tests/test_pass_through.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field, make_dataclass
+from typing import Any, Dict, List, Optional
+from uuid import UUID
+
+from pytest import mark
+
+from apischema import alias, properties
+from apischema.metadata import flattened, skip
+from apischema.serialization import PassThroughOptions, pass_through
+from apischema.utils import to_camel_case
+
+
+@dataclass
+class Flattened:
+    field: int
+
+
+@mark.parametrize(
+    "option_and_field",
+    [
+        ("aliaser", ("with_underscore", int)),
+        ("aliased_fields", ("aliased", int, field(metadata=alias("toto")))),
+        ("flattened_fields", ("flattened", Flattened, field(metadata=flattened))),
+        (
+            "properties_fields",
+            ("properties", Dict[str, int], field(metadata=properties)),
+        ),
+        ("skipped_fields", ("skipped", int, field(metadata=skip))),
+    ],
+)
+@mark.parametrize("has_field", [False, True])
+@mark.parametrize("has_option", [False, True])
+@mark.parametrize("uuid_field", [False, True])
+def test_pass_through_dataclasses(option_and_field, has_field, has_option, uuid_field):
+    fields: List[Any] = [("field", int)]
+    if has_field:
+        fields.append(option_and_field[1])
+    if uuid_field:
+        fields.append(("id", UUID))
+    cls = make_dataclass("Data", fields)
+    rec_cls = make_dataclass("RecData", [("rec", ...)], bases=(cls,))
+    rec_cls.__annotations__["rec"] = Optional[rec_cls]
+    options = PassThroughOptions(
+        dataclasses=PassThroughOptions.Dataclass(**{option_and_field[0]: has_option})
+    )
+    pass_through_result = not uuid_field and (has_option or not has_field)
+    assert (
+        pass_through(cls, aliaser=to_camel_case, options=options)
+        == pass_through(rec_cls, aliaser=to_camel_case, options=options)
+        == pass_through_result
+    )
