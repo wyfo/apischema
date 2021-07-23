cdf3380a3d53113c8ce41eaf1d8c5b44241f16f9
Joseph Perez <joperez@hotmail.fr>
2021-07-23 23:43:17 +0200

    Fix test for py36/py37

--- tests/integration/test_object_fields_overriding.py
+++ tests/integration/test_object_fields_overriding.py
@@ -1,7 +1,8 @@
+import sys
 from dataclasses import dataclass, replace
 from typing import Optional
 
-from pytest import raises
+from pytest import mark, raises
 
 from apischema import ValidationError, deserialize, serialize
 from apischema.metadata import none_as_undefined
@@ -13,6 +14,7 @@ class Foo:
     bar: Optional[str] = None
 
 
+@mark.skipif(sys.version_info < (3, 8), reason="dataclasses.replace bug with InitVar")
 def test_object_fields_overriding():
     set_object_fields(Foo, [])
     assert serialize(Foo, Foo()) == {}
