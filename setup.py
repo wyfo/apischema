a291b6d8db7c225ad9bd9796c095553791efc6ee
Joseph Perez <joperez@hotmail.fr>
2021-12-06 07:47:22 +0100

    Bump version v0.16.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="apischema",
-    version="0.16.3",
+    version="0.16.4",
     url="https://github.com/wyfo/apischema",
     author="Joseph Perez",
     author_email="joperez@hotmail.fr",
