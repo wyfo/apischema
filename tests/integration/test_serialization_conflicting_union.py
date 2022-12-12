2932b3eb4b3946ab87bd06e7613ac32ac718215b
Joseph Perez <joperez@hotmail.fr>
2022-12-12 04:09:27 +0100

    Fix UncheckedTuples not failing for wrong amount of arguments (#521)
    Thanks to @Germandrummer92

---
+++ tests/integration/test_serialization_conflicting_union.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Tuple, Union
+
+import apischema
+
+
+@dataclass(frozen=True)
+class SomeTupleClass:
+    bar: Union[Tuple[int, int], Tuple[int, int, int]]
+
+
+def test_correct_serialization() -> None:
+    serialized_dict = {"bar": [0, 0, 0]}
+    as_python_object = apischema.deserialize(type=SomeTupleClass, data=serialized_dict)
+    assert as_python_object == SomeTupleClass(bar=(0, 0, 0))
+    assert apischema.serialize(as_python_object) == serialized_dict
